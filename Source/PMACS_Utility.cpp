f5b4373e8b0156babad033688dd3cc2f2de11fe9
Erik Johnson <erik@distractal.com>
2017-04-22 18:48:57 -0400

    Completed getSequenceNumber

---
+++ Source/PMACS_Utility.cpp
@@ -0,0 +1,6 @@
+#include "PMACS_Globals.h"
+
+int getSequenceNumber(int seq_index)
+{
+	return sequenceNumber[seq_index];
+}
---
+++ Source/PMACS_Utility.h
@@ -0,0 +1,6 @@
+#ifndef PMACS_UTILITY_H
+#define PMACS_UTLITY_H
+
+int getSequenceNumber(int seq_index);
+
+#endif
