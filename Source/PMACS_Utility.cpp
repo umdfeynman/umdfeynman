af0aa398b0bf1bbe56cf615a8e1c37c016751a63
durwinj <durwinj@umich.edu>
2017-04-26 10:54:08 -0400

    Alternate store item find utilities

--- Source/PMACS_Utility.cpp
+++ Source/PMACS_Utility.cpp
@@ -22,6 +22,7 @@ int findStore(int store_number)
 	return -1;
 }
 
+
 int findStoreItem(int item_number)
 {
 	for (int i = 0; i < store_inventory_table.size(); i++)
@@ -42,4 +43,35 @@ int findWarehouseItem(int item_number)
 	}
 
 	return -1;
+}
+
+//give number and store
+int findStoreItem(int item_number, int storeNumber)
+{
+	for (int i = 0; i < store_inventory_table.size(); i++)						//for all the store items
+	{
+		if (store_inventory_table[i].store_number == storeNumber)				//match store
+		{
+			if (store_inventory_table[i].item_number == item_number)			//match item
+			{
+				return i;														//return index
+			}
+		}
+	}
+	return -1;
+}
+
+//give itemnumber and character to get list of all containing the item
+vector<int> getStoreItemList(int item_number)
+{
+	vector<int> list; 
+
+	for (int i = 0; i < store_inventory_table.size(); i++)						//for all the store items
+	{
+		if (store_inventory_table[i].item_number == item_number);				//match number
+		{
+			list.push_back(i);													//add index to list
+		}
+	}
+	return list;
 }
--- Source/PMACS_Utility.h
+++ Source/PMACS_Utility.h
@@ -6,4 +6,7 @@ int getRecordLength(int rec_index);
 int findStore(int store_number);
 int findWarehouseItem(int item_number);
 int findStoreItem(int item_number);
+int findStoreItem(int item_number, int storeNumber);
+vector<int> getStoreItemList(int item_number);
+
 #endif
